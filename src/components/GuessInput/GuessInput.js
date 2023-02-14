52e65fc34268eb5dda64a4b46a7b131d3ff56cca
Helder Burato Berto <helder.burato@gmail.com>
2023-02-14 12:52:30 +0000

    Validate min 5 characters guess

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -6,6 +6,11 @@ function GuessInput() {
   function handleSubmit(event) {
     event.preventDefault();
 
+    if (guess.length !== 5) {
+      window.alert('Please enter a 5 letter word');
+      return;
+    }
+
     console.log({ guess });
     setGuess('');
   }
@@ -14,14 +19,14 @@ function GuessInput() {
     <form onSubmit={handleSubmit} className="guess-input-wrapper">
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
+        minLength={5}
+        maxLength={5}
         value={guess}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
           setGuess(nextGuess);
         }}
-        required
-        minLength="5"
-        maxLength="5"
         type="text"
         id="guess-input"
       />
