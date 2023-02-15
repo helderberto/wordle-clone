fb45a2e90b3d266f881cea394b7eacaa9e69586c
Helder Burato Berto <helder.burato@gmail.com>
2023-02-15 12:50:59 +0000

    Add Keyboard support

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { checkGuess } from '../../game-helpers';
+
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
-import LostBanner from '../LostBanner';
 import WonBanner from '../WonBanner';
-
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+import LostBanner from '../LostBanner';
+import Keyboard from '../Keyboard';
 
 function Game() {
+  const [answer, setAnswer] = React.useState(() => sample(WORDS));
+
   // running | won | lost
   const [gameStatus, setGameStatus] = React.useState('running');
+
   const [guesses, setGuesses] = React.useState([]);
 
   function handleSubmitGuess(tentativeGuess) {
     const nextGuesses = [...guesses, tentativeGuess];
     setGuesses(nextGuesses);
 
-    // This is redundant, because I convert to uppercase before submit the guess.
     if (tentativeGuess.toUpperCase() === answer) {
       setGameStatus('won');
     } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
@@ -30,16 +30,33 @@ function Game() {
     }
   }
 
+  function handleRestart() {
+    const newAnswer = sample(WORDS);
+    setAnswer(newAnswer);
+    setGuesses([]);
+    setGameStatus('running');
+  }
+
+  const validatedGuesses = guesses.map((guess) => checkGuess(guess, answer));
+
   return (
     <>
-      {gameStatus}
-      <GuessResults guesses={guesses} answer={answer} />
+      <GuessResults validatedGuesses={validatedGuesses} />
       <GuessInput
         gameStatus={gameStatus}
         handleSubmitGuess={handleSubmitGuess}
       />
-      {gameStatus === 'lost' && <LostBanner answer={answer} />}
-      {gameStatus === 'won' && <WonBanner numOfGuesses={guesses.length} />}
+      <Keyboard validatedGuesses={validatedGuesses} />
+
+      {gameStatus === 'won' && (
+        <WonBanner
+          numOfGuesses={guesses.length}
+          handleRestart={handleRestart}
+        />
+      )}
+      {gameStatus === 'lost' && (
+        <LostBanner answer={answer} handleRestart={handleRestart} />
+      )}
     </>
   );
 }
